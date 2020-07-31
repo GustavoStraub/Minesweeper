2c4bf28d3a63384082e23d4e706eec4f0c0a38c7
Gustavo Straub <gustavostraubcovers@gmail.com>
2020-07-31 19:10:51 -0300

    logic

--- src/components/functions.js
+++ src/components/functions.js
@@ -34,3 +34,57 @@ export const createMinedBoard = (rows, columns, minesAmount) => {
   spreadMines(board, minesAmount)
   return board
 }
+
+export const cloneBoard = board => {
+  return board.map(rows => {
+    return rows.map((field) => {
+      return { ...field }
+    })
+  })
+}
+
+const getNeighbors = (board, row, column) => {
+  const neighbors = []
+  const rows = [row - 1, row, row + 1]
+  const columns = [column - 1, column, column + 1]
+  rows.forEach(r => {
+    columns.forEach(c => {
+      const different = r !== row || c !== column
+      const validRow = r >= 0 && board.length
+      const validColumn = c >= 0 && board[0].length
+      if (different && validRow && validColumn) {
+        neighbors.push(board[r][c])
+      }
+    })
+  })
+  return neighbors
+}
+
+const safeNeighborhood = (board, row, column) => {
+  const safes = (result, neighbor) => result && !neighbor.mined
+  return getNeighbors(board, row, column).reduce(safes, true)
+}
+
+export const openField = (board, row, column) => {
+  const field = board[row][column]
+  if (!field.opened) {
+    field.opened = true
+    if (field.mined) {
+      field.exploded = true
+    } else if (safeNeighborhood(board, row, column)) {
+      getNeighbors(board, row, column)
+        .forEach(n => openField(board, n.row, n.column))
+    } else {
+      const neighbors = getNeighbors(board, row, column)
+      field.nearMines = neighbors.filter(n => n.mined).lenght
+    }
+  }
+}
+
+const fields = board => [].concat(...board)
+export const hadExplosion = board => fields(board).filter(field => field.exploded).length > 0
+const pendding = field => (field.mined && !field.flagged)
+  || (!field.mied && !field.opened)
+export const wonGame = board => fields(board).filter(pendding).lenght === 0
+export const showMines = board => fields(board)
+  .filter(field => field.mined).forEach(field => field.opened = true)
