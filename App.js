ff2f0e9cdd6e735fd9121035a7668a35f5b8c616
Gustavo Straub <gustavostraubcovers@gmail.com>
2020-07-30 23:42:53 -0300

    Field

---
+++ App.js
@@ -0,0 +1,86 @@
+
+import React from 'react'
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native'
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen'
+
+import params from './src/params'
+import Field from './src/components/Field'
+
+const App = (props) => {
+  
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <View style={styles.container}>
+          <Text style={styles.welcome}>Inciando o Mines!</Text>
+          <Text style={styles.welcome}>Tamanho da grade:
+          {params.getRowsAmount()}x{params.getColumnsAmount()}
+          </Text>
+          <Field />
+          <Field opened/>
+          <Field opened nearMines ={6}/>
+        </View>
+      </SafeAreaView>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+  body: {
+    backgroundColor: Colors.white,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.dark,
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  footer: {
+    color: Colors.dark,
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
+  },
+});
+
+export default App;
